37968ad16ae86cd7e2759121ee71b6a786312c95
Nicolas DUBIEN <github@dubien.org>
2014-05-02 18:49:33 +0100

    More shared memory for shared technic
    The shape for the CHECKBOARD_BLOCK has been changed a bit: 16x16 -> 1x256
    Shared memory now keeps a copy of d_v (the vector to multiply). This modification is very useful and decrease drastically the runtime of this procedure.

--- main.cu
+++ main.cu
@@ -4,15 +4,15 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define WIDTH 1024
-#define HEIGHT 256
+#define WIDTH 2048
+#define HEIGHT 2048
 #define NUM_TESTS 1000
 
 #define CPU 1
 #define GPU 0
 
-#define CHECKBOARD_BLOCK_MAX_WIDTH  16
-#define CHECKBOARD_BLOCK_MAX_HEIGHT 16
+#define CHECKBOARD_BLOCK_MAX_WIDTH  1
+#define CHECKBOARD_BLOCK_MAX_HEIGHT 256
 
 class Matrix {
 private:
@@ -271,18 +271,19 @@ Vector productMatrixVectorGPU_naive(const Matrix &h_m, const Vector &h_v) {
 }
 
 __global__ void productMatrixVectorGPU_shared_kernel(const Matrix d_m, const Vector d_v, Vector d_r) {
-	extern __shared__ float block_result[]; // best value: blockDim.x
-	
-	block_result[threadIdx.x] = 0;
-	__syncthreads();
+	extern __shared__ float block_result[]; // best value: blockDim.x + blockDim.y
 	
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
 	int j = blockIdx.y * blockDim.y + threadIdx.y;
 	
+	block_result[threadIdx.x] = 0;
+	block_result[blockDim.x + threadIdx.y] = d_v[j];
+	__syncthreads();
+	
 	if (i >= d_m.getHeight() || j >= d_m.getWidth())
 		return;
 	
-	atomicAdd(&block_result[threadIdx.x], d_m.get(i, j) * d_v[j]);
+	atomicAdd(&block_result[threadIdx.x], d_m.get(i, j) * block_result[blockDim.x + threadIdx.y]);
 	__syncthreads();
 	
 	if (threadIdx.y == 0)
@@ -308,7 +309,7 @@ Vector productMatrixVectorGPU_shared(const Matrix &h_m, const Vector &h_v) {
 	
 	const dim3 num_threads(CHECKBOARD_BLOCK_MAX_HEIGHT, CHECKBOARD_BLOCK_MAX_WIDTH, 1);
 	const dim3 num_blocks((d_m.getHeight() + CHECKBOARD_BLOCK_MAX_HEIGHT -1)/CHECKBOARD_BLOCK_MAX_HEIGHT, (d_m.getWidth() + CHECKBOARD_BLOCK_MAX_WIDTH -1)/CHECKBOARD_BLOCK_MAX_WIDTH, 1);
-	productMatrixVectorGPU_shared_kernel<<<num_blocks, num_threads, CHECKBOARD_BLOCK_MAX_HEIGHT>>>(d_m, d_v, d_r);
+	productMatrixVectorGPU_shared_kernel<<<num_blocks, num_threads, CHECKBOARD_BLOCK_MAX_HEIGHT + CHECKBOARD_BLOCK_MAX_WIDTH>>>(d_m, d_v, d_r);
 	
 	Vector h_r(d_r, CPU);
 	return h_r;
@@ -333,9 +334,9 @@ int main(int argc, char **argv) {
 	
 	clock_t t_chrono;
 	t_chrono = clock();
-	for (int i(0) ; i != NUM_TESTS ; i++) {
+	/*for (int i(0) ; i != NUM_TESTS ; i++) {
 		productMatrixVectorCPU(h_m, h_v);
-	}
+	}*/
 	t_chrono = clock() - t_chrono;
 	std::cout << "Measured time for <CPU>: " << ((float) t_chrono)/CLOCKS_PER_SEC << "s" << std::endl;
 	
@@ -347,7 +348,7 @@ int main(int argc, char **argv) {
 	std::cout << "Measured time for <GPU naive>: " << ((float) t_chrono)/CLOCKS_PER_SEC << "s" << std::endl;
 	
 	t_chrono = clock();
-	for (int i(0) ; i != NUM_TESTS ; i++) {
+	for (int i(0) ; i != NUM_TESTS * 100 ; i++) {
 		productMatrixVectorGPU_shared(h_m, h_v);
 	}
 	t_chrono = clock() - t_chrono;
