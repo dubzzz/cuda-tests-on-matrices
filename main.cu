02a0dca683940732bcb3db86ad0b5fc1f850874c
Nicolas DUBIEN <github@dubien.org>
2014-05-03 21:38:05 +0100

    New GPU algorithm AND more precise measure of time

--- main.cu
+++ main.cu
@@ -8,7 +8,7 @@
 #define CPU 1
 #define GPU 0
 
-#define MINI_NUM_TESTS 16
+#define NUM_TESTS 16
 
 #define CHECKBOARD_BLOCK_MAX_WIDTH  1
 #define CHECKBOARD_BLOCK_MAX_HEIGHT 256
@@ -127,6 +127,65 @@ Vector productMatrixVectorGPU_shared(const Matrix &h_m, const Vector &h_v) throw
 	return h_r;
 }
 
+__global__ void productMatrixVectorGPU_row_kernel(const Matrix d_m, const Vector d_v, Vector d_r) {
+	// Each thread deals with only one vector element of the result
+	extern __shared__ float d_v_copy[];
+	
+	unsigned int w = d_v.getSize();
+	unsigned int start_to_fill = threadIdx.x * w / blockDim.x;
+	unsigned int end_to_fill = (threadIdx.x +1) * w / blockDim.x;
+	
+	for (unsigned int j(start_to_fill) ; j != end_to_fill ; j++) {
+		d_v_copy[j] = d_v[j];
+	}
+	__syncthreads();
+	
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	if (i >= d_m.getHeight())
+		return;
+	
+	float result_vector_element = 0;
+	for (unsigned int j(0) ; j != w ; j++) {
+		result_vector_element += d_m.get(i, j) * d_v_copy[j];
+	}
+	
+	d_r[i] = result_vector_element;
+}
+
+/*
+	Parallel implementation of Matrix-Vector product
+	for GPU
+	
+	Shared implementation:
+		Checkboard partitioning
+		Shared memory
+	
+	TODO check: m.getWidth() = v.getSize()
+*/
+Vector productMatrixVectorGPU_row(const Matrix &h_m, const Vector &h_v) throw (cudaError_t) {
+	Matrix d_m(h_m, GPU);
+	Vector d_v(h_v, GPU);
+	
+	Vector d_r(h_m.getHeight(), GPU);
+	d_r.memsetZero();
+	
+	const dim3 num_threads(MAX_NUM_THREADS, 1, 1);
+	const dim3 num_blocks((d_m.getHeight() + MAX_NUM_THREADS -1)/MAX_NUM_THREADS, 1, 1);
+	productMatrixVectorGPU_row_kernel<<<num_blocks, num_threads, d_v.getSize() * sizeof(float)>>>(d_m, d_v, d_r);
+	cudaThreadSynchronize(); // block until the device is finished
+	
+	// check for error
+	cudaError_t error = cudaGetLastError();
+  	if(error != cudaSuccess) {
+    		std::cout << "CUDA error: " << cudaGetErrorString(error) << std::endl;
+  		throw error;
+  	}
+	
+	Vector h_r(d_r, CPU);
+	return h_r;
+}
+
 void randMatrix(Matrix &h_m) {
 	for (unsigned int i(0) ; i != h_m.getHeight() ; i++) {
 		for (unsigned int j(0) ; j != h_m.getWidth() ; j++) {
@@ -182,39 +241,33 @@ bool checkResultsProductMatrixVector(Vector(*model)(const Matrix &, const Vector
 void measureProductMatrixVector(Vector(*func)(const Matrix &, const Vector &)) {
 	std::cout << "NEW MEASUREMENTS for Matrix-Vector product: " << std::endl;
 	
-	unsigned long int LAST_NUM_TESTS(MINI_NUM_TESTS);
 	for (unsigned int size(2) ; size < 10000 ; size <<= 1) {
 		Matrix h_m(size, size, CPU);
 		randMatrix(h_m);
 		
 		Vector h_v(size, CPU);
 		randVector(h_v);
-		
-		double time_required(0);
-		unsigned long int NUM_TESTS(LAST_NUM_TESTS >> 2);
-		if (NUM_TESTS < MINI_NUM_TESTS)
-			NUM_TESTS = MINI_NUM_TESTS;
-		
-		while (time_required < 1000.) {
-			clock_t t_chrono;
-			t_chrono = clock();
-			for (unsigned long int i(0) ; i != NUM_TESTS ; i++) {
-				try {		
-					func(h_m, h_v);
-				} catch(cudaError_t &e) {
-					return;
-				}
-			}
-			t_chrono = clock() - t_chrono;
-			time_required = 1000. * ((double) t_chrono)/CLOCKS_PER_SEC;
 			
-			if (time_required < 1000. && NUM_TESTS < (NUM_TESTS<<1))
-				NUM_TESTS <<= 1;
-			else
-				break;
+		float time_required(0);
+		
+		for (unsigned long int i(0) ; i != NUM_TESTS ; i++) {
+			float time(0);
+			cudaEvent_t start, stop;
+			cudaEventCreate(&start);
+			cudaEventCreate(&stop);
+			cudaEventRecord(start, 0);
+			try {		
+				func(h_m, h_v);
+			} catch(cudaError_t &e) {
+				return;
+			}
+			cudaEventRecord(stop, 0);
+			cudaEventSynchronize(stop);
+			cudaEventElapsedTime(&time, start, stop);
+			time_required += time;
 		}
+		time_required /= NUM_TESTS;
 		std::cout << "Size: " << size << "\tTests: " << NUM_TESTS << "\tTime: " << time_required << "ms" << std::endl;
-		LAST_NUM_TESTS = NUM_TESTS;
 	}
 }
 
@@ -223,10 +276,12 @@ int main(int argc, char **argv) {
 	
 	checkResultsProductMatrixVector(productMatrixVectorCPU, productMatrixVectorGPU_naive);
 	checkResultsProductMatrixVector(productMatrixVectorCPU, productMatrixVectorGPU_shared);
+	checkResultsProductMatrixVector(productMatrixVectorCPU, productMatrixVectorGPU_row);
 	
 	measureProductMatrixVector(productMatrixVectorCPU);
 	measureProductMatrixVector(productMatrixVectorGPU_naive);
 	measureProductMatrixVector(productMatrixVectorGPU_shared);
+	measureProductMatrixVector(productMatrixVectorGPU_row);
 	
 	return 0;
 }
